7fb504f5857e3162ebe19c85219ccbd531006f6b
aege-adj12 <aege-adj12@eme.eb.mil.br>
2017-07-04 12:38:16 -0300

    Megaman

---
+++ Megaman/Assets/Scripts/MoveObjeto.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveObjeto : MonoBehaviour {
+
+    public float speed;
+    public float x;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        x = transform.position.x;
+
+        x += speed * Time.deltaTime;
+
+        transform.position = new Vector3(x, transform.position.y, transform.position.z);
+
+        if(x <= -7)
+        {
+            Destroy(transform.gameObject);
+        }
+	}
+}
---
+++ Megaman/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public Animator Anime;
+    public Rigidbody2D playerRigidbody;
+    public int forceJump;
+
+
+    public bool slide;
+
+    //verifica o chao
+    public Transform GroundCheck;
+    public bool grounded;
+    public LayerMask WhatIsGround;
+
+    //slide
+    public float slideTemp;
+    public Transform colisor;
+    public float timeTemp;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Jump")&& grounded == true)
+        {
+            playerRigidbody.AddForce(new Vector2(0, forceJump));
+            if (slide == true)
+            {
+                colisor.position = new Vector3(colisor.position.x, colisor.position.y + 0.3f, colisor.position.z);
+            }
+            slide = false;
+        }
+        if (Input.GetButtonDown("Slide")&& grounded == true && slide == false)
+        {
+            colisor.position = new Vector3(colisor.position.x, colisor.position.y - 0.3f, colisor.position.z);
+            slide = true;
+            timeTemp = 0;
+        }
+
+        grounded = Physics2D.OverlapCircle(GroundCheck.position, 0.2f, WhatIsGround);
+
+        if (slide == true)
+        {
+            timeTemp += Time.deltaTime;
+            if (timeTemp >= slideTemp)
+            {
+                colisor.position = new Vector3(colisor.position.x, colisor.position.y + 0.3f, colisor.position.z);
+                slide = false;
+            }
+        }
+        Anime.SetBool("jump", !grounded);
+        Anime.SetBool("slide", slide);
+		
+	}
+
+    private void OnTriggerEnter2D()
+    {
+        Debug.Log("Bateu");
+    }
+}
